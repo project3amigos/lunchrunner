7613b734014b038c8817e7c78f088123d6a42df3
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-04-16 19:53:45 -0600

    details routes working

---
+++ routes/api/details.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const detailsController = require("../../controllers/detailsController");
+
+router
+.route("/")
+.get(detailsController.findAll)
+.post(detailsController.create)
+
+router
+.route("/:id")
+.get(detailsController.findOne)
+.delete(detailsController.delete);
+
+module.exports = router;
