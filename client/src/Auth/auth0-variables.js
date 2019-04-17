99d8b7764e57b3e33efdee2d010fb3155b374678
variable56 <variable56@gmail.com>
2019-04-16 19:55:28 -0600

    fixed callback URL

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'project3amigos.auth0.com',
   clientId: 'aO3VTrwPSzq4pFruLMLJ9BNrm1DEKA1H',
-  callbackUrl: window.location.host.includes("localhost") ? "http://localhost:3000/callback": "https://project3amigos.herokuapp.com/callback"
+  callbackUrl: window.location.host.includes("localhost") ? "http://localhost:3000/callback": "https://project-3-amigos-lunch-runner.herokuapp.com/callback"
 }
